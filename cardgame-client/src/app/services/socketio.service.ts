09cf8dc1eeaa107e1e1a24e8c5f470fdc29306cb
Jordan <jbrockerrudow@gmail.com>
2022-08-26 13:02:42 -0500

    setupSocketConnection and disconnect services

--- cardgame-client/src/app/services/socketio.service.ts
+++ cardgame-client/src/app/services/socketio.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { DefaultEventsMap } from '@socket.io/component-emitter';
+import { io, Socket } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketioService {
 
-  constructor() { }
+  socket;
+
+  constructor() {   }
+
+  setupSocketConnection() {
+    this.socket = io(environment.SOCKET_ENDPOINT, {
+      auth: {
+        token: "abc"
+      }
+    });
+
+    this.socket.emit('my message', 'Hello there from Angular.');
+
+    this.socket.on('my broadcast', (data: string) => {
+      console.log(data);
+    });
+  }
+  
+  disconnect() {
+    if (this.socket) {
+      this.socket.disconnect();
+    }
+  }
 }
