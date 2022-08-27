960a09b7f649eebd6218c3ce9451cbd7d57a7199
Jordan <jbrockerrudow@gmail.com>
2022-08-27 08:39:28 -0500

    Add Models, Services, and Required Imports

--- cardgame-client/src/app/services/deck-of-cards.service.ts
+++ cardgame-client/src/app/services/deck-of-cards.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay, map, catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { DeckOfCardsAPI } from '../shared/baseurl';
+import { HttpMsgService } from './http-msg.service';
+
+import { CARDS } from '../shared/cards';
+import { Card } from '../models/card';
+import { CardDeck } from '../models/card-deck';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeckOfCardsService {
 
-  constructor() { }
+  constructor(
+    private httpMsgService: HttpMsgService
+  ) { }
 }
