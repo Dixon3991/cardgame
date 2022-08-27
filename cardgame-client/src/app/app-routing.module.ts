0c54278f7ddfd6648442566d72272f510b122009
Jordan <jbrockerrudow@gmail.com>
2022-08-27 09:40:07 -0500

    Add initial routing

--- cardgame-client/src/app/app-routing.module.ts
+++ cardgame-client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameRoomComponent } from './components/game-room/game-room.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'join', component: GameRoomComponent },
+  //{ path: '**', component: <component-name> },
+  //{ path: '',   redirectTo: '/first-component', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
