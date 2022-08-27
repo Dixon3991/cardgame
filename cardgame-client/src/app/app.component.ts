8f263e07096888f17f23e95c5ca0873e294815c1
Jordan <jbrockerrudow@gmail.com>
2022-08-27 07:31:16 -0500

    Integrate socketService

--- cardgame-client/src/app/app.component.ts
+++ cardgame-client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketioService } from './services/socketio.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cardgame-client';
+
+  constructor(private socketService: SocketioService) {}
+  
+  ngOnInit() {
+    this.socketService.setupSocketConnection();
+  }
+
+  ngOnDestroy() {
+    this.socketService.disconnect();
+  }
+
+  
+
+
 }
