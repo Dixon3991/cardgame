0b85c0ec9bee0d4425b5cae79c923c45ddc2d518
Jordan <jbrockerrudow@gmail.com>
2022-11-12 09:13:43 -0600

    Create Src Directory

---
+++ src/Card.cpp
@@ -0,0 +1,59 @@
+#include "Card.h"
+#include <iostream>
+#include "TColor.h"
+
+Card::Card(Ranks RANK, Suits SUIT)
+    : Rank(RANK),
+      Suit(SUIT)
+{
+    //Nothing here...
+}
+
+Card::~Card()
+{
+    //Nothing here... memory leak gaurd
+}
+
+Card::Ranks Card::GetRank()
+{
+    return Rank;
+}
+
+Card::Suits Card::GetSuit()
+{
+    return Suit;
+}
+
+int Card::GetFaceValue()
+{
+    if (Rank <= TEN)
+        return static_cast<int>(Rank);
+    if(Rank <= KING)
+        return 10;
+        
+    return 11;
+}
+
+void Card::Print()
+{
+    if(Rank <= TEN)
+        std::cout << Rank;
+    else if (Rank == JACK)
+        std::cout << "J";
+    else if (Rank == QUEEN)
+        std::cout << "Q";
+    else if (Rank == KING)
+        std::cout << "K";
+    else
+        std::cout << "A";
+
+
+    if (Suit == HEARTS)
+        print("♥", color_red, color_white);
+    else if (Suit == DIAMONDS)
+        print("♦", color_red, color_white);
+    else if (Suit == CLUBS)
+        print("♣", color_black, color_white);
+    else
+        print("♠", color_black, color_white);
+}
---
+++ src/Deck.cpp
@@ -0,0 +1,41 @@
+#include "Deck.h"
+#include <algorithm>
+#include <iostream>
+
+Deck::Deck()
+    : deck()
+{
+    deck.reserve(52);
+    for (Card::Ranks r = Card::TWO; r <= Card::ACE; r = static_cast<Card::Ranks>(static_cast<int>(r)+1))
+    {
+        for (Card::Suits s = Card::HEARTS; s <= Card::SPADES; s = static_cast<Card::Suits>(static_cast<int>(s)+1))
+        {
+            deck.push_back(new Card(r, s));
+        }
+    }
+}
+
+Deck::~Deck()
+{
+    for(Card* card : deck)
+    {
+        delete card;
+    }
+    deck.clear();
+
+}
+
+void Deck::Print()
+{
+    for(Card* card : deck)
+    {
+        card -> Print();
+        std::cout << " ";
+    }
+    std::cout << std::endl << std::endl;
+}
+
+void Deck::Shuffle()
+{
+    std::random_shuffle(deck.begin(), deck.end());
+}
